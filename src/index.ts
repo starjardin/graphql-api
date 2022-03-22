1919645041f04df36fbae56239326a324e744e9e
Tantely <tantely.and@onja.org>
2022-03-22 17:26:27 +0300

    adds a database

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
-import 'reflect-metadata';
-import { createConnection } from 'typeorm';
-import { ApolloServer } from 'apollo-server';
-import { buildSchema } from 'type-graphql';
-import { BookResolver } from './resolvers/bookResolvers';
-import { UserResolver } from './resolvers/userResolver';
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+import { BookResolver } from "./resolvers/bookResolvers";
+import { UserResolver } from "./resolvers/userResolver";
 
-(async function main () {
-  await createConnection()
+(async function main() {
+  await createConnection({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "tantely",
+    password: "admin123",
+    database: "testdatabase",
+    entities: ["src/**/*.*"],
+  });
   const schema = await buildSchema({
-    resolvers: [BookResolver, UserResolver]
-  })
-  const server = await new ApolloServer({ schema })
-  await server.listen(4000)
-  console.log("server has started")
-})()
+    resolvers: [BookResolver, UserResolver],
+  });
+  const server = await new ApolloServer({ schema, introspection: true });
+  const port = process.env.PORT || 4000
+
+  await server.listen(4000);
+
+  console.log(`server has started on port ${port}`);
+  
+})();
